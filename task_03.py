df5dc25ef67f64b4f599828a82b190dff3532cb8
paisely65 <cunysps@gmail.com>
2016-03-10 18:15:50 -0500

    Task 3 is completed

--- task_03.py
+++ task_03.py
@@ -7,7 +7,7 @@ import decimal as dec
 
 def lexicographics(to_analyze):
 
-    
+
     """ A function that calculates the maximun, minimum and average.
 
     Args:
@@ -26,9 +26,9 @@ def lexicographics(to_analyze):
     to_analyze = to_analyze.split('/n')
     average_words = dec.Decimal(0.0)
     word_count_list = []
-    for word_count in to_analyze:
-        word_count = len(to_analyze[loop_count].split())
-        word_count_list += [word_count]
-        loop_count += 1
-    average_words = sum(word_count_list) / total_statements
-    return max(word_count_list), min(word_count_list), average_words
+    count = 0
+    for word_count in (to_analyze):
+        word_count_list.append(len(line.split()))
+        count = word_count
+    return max(word_count_list), min(word_count_list), \
+           decimal.Decimal(sum(word_coun_list)) / (count + 1)
