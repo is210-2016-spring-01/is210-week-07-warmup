d41e8bb0262213e0510a225b08921e607b9b031f
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-03-10 20:12:49 +0000

    Task 3 revised

--- task_03.py
+++ task_03.py
@@ -16,13 +16,17 @@ def lexicographics(to_analyze):
 
     Examples:
         >>> import data
-    >>> lexicographics(data.SHAKESPEARE)
-    (12, 5, Decimal('8.14'))
-    >>> lexicographics('Feelings\nNothing more than\nFeelings')
-    (3, 1, Decimal('1.666666666666666666666666667'))
+        >>> lexicographics(data.SHAKESPEARE)
+        (12, 5, Decimal('8.14'))
+
+        >>> import data
+        >>> lexicographics('Feelings\nNothing more than\nFeelings')
+        (3, 1, Decimal('1.666666666666666666666666667'))
     """
     to_analyze = to_analyze.split('\n')
     wordct = []
+    count = 0
     for number, line in enumerate(to_analyze):
         wordct.append(len(line.split()))
-    return max(wordct), min(wordct), decimal.Decimal(sum(wordct)) / (number + 1)
+        count = number
+    return max(wordct), min(wordct), decimal.Decimal(sum(wordct)) / (count + 1)
