5835ed8cd7df1d0f78481c684711d0d76a7cd7c8
paisely65 <cunysps@gmail.com>
2016-03-10 15:18:21 -0500

    THe Bool has beenmodified

---
+++ task_02.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Statement to show truthiness."""
+
+
+def bool_to_str(bval):
+"""A function that returns yes/no string.
+
+    Args:
+        bval(bool), required: Truthy of Falsy.
+
+    Returns:
+        result: Yes or no string based on the bool value.
+
+    Example:
+    >>> bool_to_str(True)
+    'Yes'
+
+    >>> bool_to_str('')
+    'No'
+    """
+    if bval:
+        result = 'Yes'
+    else:
+        result = 'No'
+
+        return result
