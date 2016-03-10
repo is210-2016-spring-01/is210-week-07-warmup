b1ce2f209e6c52b0c1dbfabf89eda9738eb4a650
paisely65 <cunysps@gmail.com>
2016-03-10 17:58:12 -0500

    Four errors addressed in this file.

--- task_03.py
+++ task_03.py
@@ -6,6 +6,8 @@
 import decimal as dec
 
 def lexicographics(to_analyze):
+
+    
     """ A function that calculates the maximun, minimum and average.
 
     Args:
@@ -22,15 +24,11 @@ def lexicographics(to_analyze):
     (12, 5, DECIMAL('8.14'))
     """
     to_analyze = to_analyze.split('/n')
-    total_statement = dec.Decimal(len(to_analyze))
-    loop_count, word_count = 0
     average_words = dec.Decimal(0.0)
     word_count_list = []
     for word_count in to_analyze:
         word_count = len(to_analyze[loop_count].split())
         word_count_list += [word_count]
         loop_count += 1
-
-
     average_words = sum(word_count_list) / total_statements
     return max(word_count_list), min(word_count_list), average_words
